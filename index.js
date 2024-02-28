f8a0eb65e729e2695ded440eb4da14327b801798
Damaride Putignano <damarideputignano@gmail.com>
2024-02-28 17:39:22 +0100

    Added array for future cart

--- index.js
+++ index.js
@@ -10,52 +10,60 @@ fetch("https://striveschool-api.herokuapp.com/books")
 
   .then((booksData) => {
     console.log("BOOKS DATA", booksData);
-    console.log("BOOKS IMG", booksData[0].img);
-    console.log("BOOKS IMG", booksData[2].img);
+    // console.log("BOOKS IMG", booksData[0].img);
+    // console.log("BOOKS IMG", booksData[2].img);
 
     const container = document.getElementById("bookshelf");
-
+    const cartListArr = [];
 
     booksData.forEach((book) => {
       const singleCard = document.createElement("div");
       const img = document.createElement("img");
       const cardBody = document.createElement("div");
-   
+
       const imgURL = book.img;
       img.src = imgURL;
 
       const title = document.createElement("p");
-      title.innerText = book.title
+      title.innerText = book.title;
 
       const price = document.createElement("p");
       price.innerText = "Price: " + book.price;
 
       const deleteBtn = document.createElement("button");
-      deleteBtn.innerText = "scarta"
+      deleteBtn.innerText = "scarta";
 
-      deleteBtn.addEventListener("click", function(event) {
+      deleteBtn.addEventListener("click", function (event) {
         singleCard.classList.add("d-none");
-        })
+      });
 
-      
+      const buyBtn = document.createElement("button");
+      buyBtn.innerText = "buy";
+
+      buyBtn.addEventListener("click", function (event) {
+        cartListArr.push(book);
+        console.log(cartListArr);
+      });
+
       container.appendChild(singleCard);
       singleCard.appendChild(img);
-      singleCard.appendChild(cardBody)
+      singleCard.appendChild(cardBody);
       cardBody.appendChild(title);
       cardBody.appendChild(price);
       cardBody.appendChild(deleteBtn);
+      cardBody.appendChild(buyBtn);
 
-      deleteBtn.classList.add("deleteStyle")
-      
+      deleteBtn.classList.add("deleteStyle");
+
       singleCard.classList.add("card");
       singleCard.classList.add("col-4");
       singleCard.classList.add("col-md-3");
       singleCard.classList.add("col-lg-2");
-      
-      img.classList.add("imgStyle")
-      cardBody.classList.add("cardbody")
-      
-      console.log("BOOKS DATA", booksData);
+
+      img.classList.add("imgStyle");
+      cardBody.classList.add("cardbody");
+
+      // console.log("BOOKS DATA", booksData);
     });
   })
 
